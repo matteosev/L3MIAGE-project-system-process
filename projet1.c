9fcf40d09d53bba30dcc678646ca2bdefe22ff02
matteo severini <matteo@matteosev.home>
2023-10-12 22:33:05 +0200

    Pipes mis en place

--- projet1.c
+++ projet1.c
@@ -4,62 +4,62 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Pas de paramètre N (nombre de processus fils)");
-        exit(1);
-    }
+	if (argc < 2) {
+		fprintf(stderr, "Pas de paramètre N (nombre de processus fils)\n");
+		exit(1);
+	}
 
-    int N = atoi(argv[1]);
-    
-    int f_controller[2];
-    int f[N][2];
+	int N = atoi(argv[1]);
+	int pid_parent = getpid();
+	int node_num;
 
-    if (pipe(f_controller) == -1) {
-        fprintf(stderr, "Erreur de pipe\n");
-        exit(1);
-    }
-    
-    for (int i = 0; i < N; i++) {
+	int f_controller[2];
+	int f[N][2];
 
-        // Le processus i initialise le pipe qui le relie au processus i+1
-        // Le dernier processus ne communique avec aucun autre processus (i < N-1)
-        if (i < N-1 && pipe(f[i]) == -1) {
-            fprintf(stderr, "Erreur de pipe au processus n°%d\n", i);
-            exit(1);
-        }
+	if (pipe(f_controller) == -1) {
+		fprintf(stderr, "Erreur de pipe\n");
+		exit(1);
+	}
 
-        switch(fork()) {
-            case -1:
-                fprintf(stderr, "Erreur de fork\n"); exit(1);
-            case 0:
-                close(f_controller[0]);
-                close(f[i][1]);
+	// Le pipe i relie le node i au node i+1
+	// Exception : le pipe N-1 relie le node N-1 au node 0
+	for (int i = 0; i < N; i++) {
+		if (pipe(f[i]) == -1) {
+			fprintf(stderr, "Erreur de pipe\n", i);
+			exit(1);
+		}
+	}
 
-                // Le processus ferme le pipe de son prédécesseur en écriture
-                if (i > 0)
-                    close(f[i - 1][1]);
+	// Création des N processus fils
+	for (int i = 0; i < N; i++) {
+		switch(fork()) {
+			case -1:
+				fprintf(stderr, "Erreur de fork\n"); exit(1);
+			case 0:
+				close(f_controller[0]);
+				close(f[i][0]);
+				node_num = i;
+				i = N;	// On quitte le for
+		}
+	}
 
-                printf("Fils pid %d\n", getpid());
-                close(f_controller[1]);
+	if (getpid() == pid_parent) {
+		while(wait(NULL) != -1)
+			;
 
-                if (i > 0)
-                    close(f[i - 1][0]);
-            default:
-                close(f_controller[1]);
+		printf("Tous les fils créés et morts\n");
+	}
+	else {
+		printf("Fils pid %d n°%d\n", getpid(), node_num);
+	}
 
-                // Le processus père ferme tous les pipe fils en lecture
-                for (int y = 0; y < N; y++)
-                    close(f[y][0]);
+	close(f_controller[0]);
+	close(f_controller[1]);
 
-                while(wait(NULL) != -1);
+	for (int i = 0; i < N; i++) {
+		close(f[i][0]);
+		close(f[i][1]);
+	}
 
-                printf("Tous les fils créés et morts\n");
-
-                for (int y = 0; y < N; y++)
-                    close(f[y][1]);
-
-                close(f_controller[0]);
-        }
-    }
-    return 0;
+	return 0;
 }
