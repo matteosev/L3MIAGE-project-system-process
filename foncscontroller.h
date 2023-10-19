383b22f02ed2178dddff2e68d857099c1d27362b
Errmi <116873286+Errmi@users.noreply.github.com>
2023-10-19 10:22:47 +0200

    Create foncscontroller.h

---
+++ foncscontroller.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "table.h"
+
+
+int numprocess(int N , int K);
+int get_command();
+int get_key();
+
+int get_string_stdin(char *s, int n);
+
+
+
+
